0493344124dd39e93e7537f84abd8cfe859f6351
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-05-01 04:34:53 -0400

    passes tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i in range(len(names)):
+        print(f'{i + 1}. {names[i]}')
+        i += 1
+        
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(names):
+    list = [f'{name.title()}!' for name in names]
+    return list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    list = [True for word in words if len(word) > 4 ]
+    if list:
+        return True
+    else:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(lists):
+    for list in lists:
+        if list == 'cheddar' or list == 'gouda' or list == 'camembert':
+            return list
+        
